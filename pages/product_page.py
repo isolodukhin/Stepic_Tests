a6bbe82d8ecbf42ca3d23cc0fd48d346adfa297b
Ilya <lokoilya@yandex.ru>
2020-11-15 10:32:27 +0300

    Блок 4.3 Задание на параметризацию

---
+++ pages/product_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        self.browser.find_element(*ProductPageLocators.ADD_BASKET).click()
+
+    def should_be_message_about_adding(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
+        # Проверяем, что название товара присутствует в сообщении о добавлении
+        # Это можно было бы сделать с помощью split() и сравнения строк,
+        # Но не вижу необходимости усложнять код
+        assert product_name in message, "No product name in the message"
+
+    def should_be_message_basket_total(self):
+        message_basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        # Проверяем, что цена товара присутствует в сообщении со стоимостью корзины
+        assert product_price in message_basket_total, "No product price in the message"
+
---
+++ test_product_page.py
@@ -0,0 +1,17 @@
+from .pages.product_page import ProductPage
+import time
+import pytest
+
+
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_message_about_adding()
+    page.should_be_message_basket_total()
+    time.sleep(1)
